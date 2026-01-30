30621acc824d4c7dff25a20487cce4d1ef74caa1
khai101280 <khai101280@donga.edu.vn>
2026-01-30 04:00:43 +0000

    update

--- node-tt/node-intern-demo/app.js
+++ node-tt/node-intern-demo/app.js
@@ -1,20 +1,25 @@
+console.log("APP PATH:", __dirname);
 require("dotenv").config();
 const express = require("express");
+
+const app = express();
+
+// middleware
+app.use(express.json());
 
 const logger = require("./middlewares/logger.middleware");
 app.use(logger);
 
+// routes
 const userRoute = require("./src/routes/user.route");
 app.use("/api/users", userRoute);
 
-
-const app = express();
-app.use(express.json());
-
+// test api
 app.get("/api/test", (req, res) => {
   res.status(200).json({ message: "Express working" });
 });
 
+// start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
